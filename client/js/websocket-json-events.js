b5deb1e38a6fc97cf40724170e7e83830a0c6ca8
Alexandru Scvortov <scvalex@gmail.com>
2012-02-25 18:43:28 +0000

    restyle ws-json-events

--- client/js/websocket-json-events.js
+++ client/js/websocket-json-events.js
@@ -19,42 +19,50 @@ The server has to send websocket data of form
   JSON.stringify({ event: EVENT_NAME, data: DATA })
 */
 
-var FancyWebSocket = function(url){
-  if (typeof WebSocket !== 'undefined')
+var FancyWebSocket = function(url) {
+  if (typeof WebSocket !== 'undefined') {
     var conn = new WebSocket(url);
-  else if (typeof MozWebSocket !== 'undefined')
+  } else if (typeof MozWebSocket !== 'undefined') {
     var conn = new MozWebSocket(url);
-  else
+  } else {
     throw 'WebSocket seems to not be supported by this browser!';
+  }
 
   var callbacks = {};
 
-  this.bind = function(event_name, callback){
+  this.bind = function(event_name, callback) {
     callbacks[event_name] = callbacks[event_name] || [];
     callbacks[event_name].push(callback);
-    return this;// chainable
+    return this; // chainable
   };
 
-  this.send = function(event_name, event_data){
+  this.send = function(event_name, event_data) {
     var payload = JSON.stringify({event:event_name, data: event_data});
-    conn.send( payload ); // <= send JSON data to socket server
+    conn.send(payload); // <= send JSON data to socket server
     return this;
   };
 
   // dispatch to the right handlers
-  conn.onmessage = function(evt){
-    var json = JSON.parse(evt.data)
-    dispatch(json.event, json.data)
+  conn.onmessage = function(evt) {
+    var json = JSON.parse(evt.data);
+    dispatch(json.event, json.data);
   };
 
-  conn.onclose = function(){dispatch('close',null)}
-  conn.onopen = function(){dispatch('open',null)}
+  conn.onclose = function() {
+    dispatch('close',null);
+  };
 
-  var dispatch = function(event_name, message){
+  conn.onopen = function() {
+    dispatch('open',null);
+  };
+
+  var dispatch = function(event_name, message) {
     var chain = callbacks[event_name];
-    if(typeof chain == 'undefined') return; // no callbacks for this event
-    for(var i = 0; i < chain.length; i++){
-      chain[i]( message )
+    if (typeof chain == 'undefined') {
+      return; // no callbacks for this event
+    }
+    for(var i = 0; i < chain.length; i++) {
+      chain[i](message);
     }
   }
 };
