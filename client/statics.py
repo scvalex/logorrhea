5b9957fe7d25f644b209ccfb616b7567a54ad694
Niklas Hambüchen <mail@nh2.me>
2012-02-25 14:15:00 +0000

    Client: Add startup scripts

---
+++ client/statics.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+
+
+ROOT_DIR = '.'
+
+STATICS = {
+	'js/coffee-script.js': 'http://github.com/jashkenas/coffee-script/raw/master/extras/coffee-script.js',
+	'js/jquery.js': 'http://code.jquery.com/jquery.min.js',
+	'js/mootools.js': 'http://mootools.net/download/get/mootools-core-1.4.4-full-nocompat.js',
+	'js/knockout.js': 'https://github.com/downloads/SteveSanderson/knockout/knockout-2.0.0.js',
+}
+
+
+if __name__ == '__main__':
+	import staticfetcher
+	staticfetcher.Staticfetcher(STATICS, ROOT_DIR).run()
