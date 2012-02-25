fdbe2bbafe82fd9e01c2cbdadb8a014285d4c6ca
Ingrid Funie <andreeaingridf@gmail.com>
2012-02-25 16:28:08 +0000

    First skeleton of iPhone app

---
+++ iPhoneClient/logorrhea/logorrhea/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  logorrhea
+//
+//  Created by Ingrid Funie on 25/02/2012.
+//  Copyright (c) 2012 ICL. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ iPhoneClient/logorrhea/logorrheaTests/logorrheaTests.m
@@ -0,0 +1,32 @@
+//
+//  logorrheaTests.m
+//  logorrheaTests
+//
+//  Created by Ingrid Funie on 25/02/2012.
+//  Copyright (c) 2012 ICL. All rights reserved.
+//
+
+#import "logorrheaTests.h"
+
+@implementation logorrheaTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in logorrheaTests");
+}
+
+@end
