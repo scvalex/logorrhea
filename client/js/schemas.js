35d8cab14e6f317d64d45270010943b833d3522f
Niklas Hambüchen <mail@nh2.me>
2012-02-25 22:00:56 +0000

    Make schemas.js a require module

--- client/js/schemas.js
+++ client/js/schemas.js
@@ -1,109 +1,113 @@
-// TODO make it a require.js module
+define(['jsschema'], function() {
 
-var schema = jsschema.schema;
-var required = jsschema.required;
-var optional = jsschema.optional;
-var repeated = jsschema.repeated;
+  var schema = jsschema.schema;
+  var required = jsschema.required;
+  var optional = jsschema.optional;
+  var repeated = jsschema.repeated;
 
-var eventSchema = function(dataSchema) {
-  return schema(function() {
-    this.event = required('string'),
-    // TODO check if optional is all right (vs jsschema, null and {} in API spec)
-    this.data = optional(dataSchema)
+  var eventSchema = function(dataSchema) {
+    return schema(function() {
+      this.event = required('string'),
+      // TODO check if optional is all right (vs jsschema, null and {} in API spec)
+      this.data = optional(dataSchema)
+    });
+  };
+
+  var conversation_schema = schema(function() {
+    this.hash  = required('string');
+    this.topic = required('string');
   });
-};
 
-var conversation_schema = schema(function() {
-  this.hash  = required('string');
-  this.topic = required('string');
+
+  var schemas = {
+
+    'connect': eventSchema(schema(function() {
+    })),
+
+    'connect.ok': eventSchema(schema(function() {
+    })),
+
+
+    // Note that this currently is for both server and client isssued disconnect
+    'disconnect': eventSchema(schema(function() {
+    })),
+
+    'disconnect.ok': eventSchema(schema(function() {
+    })),
+
+
+    'list_channels': eventSchema(schema(function() {
+    })),
+
+    'list_channels.ok': eventSchema(schema(function() {
+      this.channels = repeated('string');
+    })),
+
+
+    'list_users': eventSchema(schema(function() {
+      this.channel = required('string');
+    })),
+
+    'list_users.ok': eventSchema(schema(function() {
+      this.channel  = required('string');
+      this.users    = repeated('string');
+    })),
+
+
+    'list_coversations': eventSchema(schema(function() {
+      this.channel = required('string');
+    })),
+
+    'list_coversations.ok': eventSchema(schema(function() {
+      this.channel       = required('string');
+      this.conversations = repeated(conversation_schema);
+    })),
+
+
+    'join': eventSchema(schema(function() {
+      this.channel = required('string');
+    })),
+
+    'join.ok': eventSchema(schema(function() {
+      this.channel = required('string');
+    })),
+
+
+    'send_channel': eventSchema(schema(function() {
+      this.channel = required('string');
+      this.message = required('string');
+    })),
+
+    'send_channel.ok': eventSchema(schema(function() {
+    })),
+
+
+    'send_conversation': eventSchema(schema(function() {
+      this.channel      = required('string');
+      this.conversation = required('string');
+      this.message      = required('string');
+    })),
+
+    'send_conversation.ok': eventSchema(schema(function() {
+    })),
+
+
+    'receive_channel': eventSchema(schema(function() {
+      this.channel = required('string');
+      this.user    = required('string');
+      this.message = required('string');
+    })),
+
+
+    'receive_conversation': eventSchema(schema(function() {
+      this.channel      = required('string');
+      this.conversation = required('string');
+      this.user         = required('string');
+      this.message      = required('string');
+    }))
+
+  };
+
+  return schemas;
+
 });
-
-
-var schemas = {
-
-  'connect': eventSchema(schema(function() {
-  })),
-
-  'connect.ok': eventSchema(schema(function() {
-  })),
-
-
-  // Note that this currently is for both server and client isssued disconnect
-  'disconnect': eventSchema(schema(function() {
-  })),
-
-  'disconnect.ok': eventSchema(schema(function() {
-  })),
-
-
-  'list_channels': eventSchema(schema(function() {
-  })),
-
-  'list_channels.ok': eventSchema(schema(function() {
-    this.channels = repeated('string');
-  })),
-
-
-  'list_users': eventSchema(schema(function() {
-    this.channel = required('string');
-  })),
-
-  'list_users.ok': eventSchema(schema(function() {
-    this.channel  = required('string');
-    this.users    = repeated('string');
-  })),
-
-
-  'list_coversations': eventSchema(schema(function() {
-    this.channel = required('string');
-  })),
-
-  'list_coversations.ok': eventSchema(schema(function() {
-    this.channel       = required('string');
-    this.conversations = repeated(conversation_schema);
-  })),
-
-
-  'join': eventSchema(schema(function() {
-    this.channel = required('string');
-  })),
-
-  'join.ok': eventSchema(schema(function() {
-    this.channel = required('string');
-  })),
-
-
-  'send_channel': eventSchema(schema(function() {
-    this.channel = required('string');
-    this.message = required('string');
-  })),
-
-  'send_channel.ok': eventSchema(schema(function() {
-  })),
-
-
-  'send_conversation': eventSchema(schema(function() {
-    this.channel      = required('string');
-    this.conversation = required('string');
-    this.message      = required('string');
-  })),
-
-  'send_conversation.ok': eventSchema(schema(function() {
-  })),
-
-
-  'receive_channel': eventSchema(schema(function() {
-    this.channel = required('string');
-    this.user    = required('string');
-    this.message = required('string');
-  })),
-
-
-  'receive_conversation': eventSchema(schema(function() {
-    this.channel      = required('string');
-    this.conversation = required('string');
-    this.user         = required('string');
-    this.message      = required('string');
-  }))
-
-};
