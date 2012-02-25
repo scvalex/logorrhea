b3403d83db6de0ab651a34aa75a3ad81c208e0f5
Alexandru Scvortov <scvalex@gmail.com>
2012-02-25 14:35:26 +0000

    add jasmine lib and test page

--- client/statics.py
+++ client/statics.py
@@ -8,6 +8,9 @@ STATICS = {
 	'js/jquery.js': 'http://code.jquery.com/jquery.min.js',
 	'js/mootools.js': 'http://mootools.net/download/get/mootools-core-1.4.4-full-nocompat.js',
 	'js/knockout.js': 'https://github.com/downloads/SteveSanderson/knockout/knockout-2.0.0.js',
+        'js/jasmine.js': 'https://raw.github.com/pivotal/jasmine/master/lib/jasmine-core/jasmine.js',
+        'js/jasmine-html.js': 'https://raw.github.com/pivotal/jasmine/master/lib/jasmine-core/jasmine-html.js',
+        'css/jasmine.css': 'https://raw.github.com/pivotal/jasmine/blob/master/lib/jasmine-core/jasmine.css',
 }
 
 
